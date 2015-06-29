f876520f36ef5196dfc47311045717e9cfb0e3eb
pzavaletac <pzavaletac@gmail.com>
2015-06-29 13:53:01 -0500

    casiObligatorioMod6

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+var authorController = require('../controllers/author_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+//router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definicion de rutas de /quizes
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', authorController.author);
 
 module.exports = router;
